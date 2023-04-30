612c2ba7944f4174eeca597735f421025d13fd53
vikassrivastava18 <vikassrinitb@gmail.com>
2023-04-30 12:27:33 +0530

    Added open api spec

--- crudproject/crudproject/urls.py
+++ crudproject/crudproject/urls.py
@@ -13,3 +13,31 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
+
+
+from django.views.generic import TemplateView
+
+urlpatterns += [
+    # ...
+    # Route TemplateView to serve Swagger UI template.
+    #   * Provide `extra_context` with view name of `SchemaView`.
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url':'openapi-schema'}
+    ), name='swagger-ui'),
+]
+
+from rest_framework.schemas import get_schema_view
+
+urlpatterns += [
+    # ...
+    # Use the `get_schema_view()` helper to add a `SchemaView` to project URLs.
+    #   * `title` and `description` parameters are passed to `SchemaGenerator`.
+    #   * Provide view name for use with `reverse()`.
+    path('openapi', get_schema_view(
+        title="Your Project",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+    # ...
+]
